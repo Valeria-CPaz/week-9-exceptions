ccfedc53ef18a89d527cf8514564d1557bfe4564
Valeria Paz <21214@student.dorset-college.ie>
2020-11-17 13:01:46 +0000

    first commit

--- src/com/val/Main.java
+++ src/com/val/Main.java
@@ -1,8 +1,23 @@
 package com.val;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer numerator: ");
+        int numerator = scanner.nextInt();
+        System.out.println("Please enter and integer denominator: ");
+        int denominator = scanner.nextInt();
+        int result = quotient(numerator, denominator);
+        System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+
+    }
+
+    public static int quotient(int numerator, int denominator) {
+
+        return numerator/ denominator;
     }
 }
