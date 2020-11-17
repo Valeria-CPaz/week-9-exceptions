1974286cea4eda692a5030ef4f541aff87781dcd
Valeria Paz <21214@student.dorset-college.ie>
2020-11-17 13:18:16 +0000

    second commit

--- src/com/val/Main.java
+++ src/com/val/Main.java
@@ -1,23 +1,40 @@
 package com.val;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter an integer numerator: ");
-        int numerator = scanner.nextInt();
-        System.out.println("Please enter and integer denominator: ");
-        int denominator = scanner.nextInt();
-        int result = quotient(numerator, denominator);
-        System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
 
+        Scanner scanner = new Scanner(System.in);
+        do {
+            try {
+                System.out.println("Please enter an integer numerator: ");
+                int numerator = scanner.nextInt();
+
+                System.out.println("Please enter and integer denominator: ");
+                int denominator = scanner.nextInt();
+
+                int result = quotient(numerator, denominator);
+                System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+
+            } catch (InputMismatchException inputMismatchException) {
+
+                System.out.printf("Error: ArithmeticException was thrown\n");
+                System.out.printf("Suggestion: You cannot enter 0\n");
+
+            } catch (ArithmeticException arithmeticException) {
+                System.out.printf("Error: ArithmeticException was thrown\n");
+                System.out.printf("Suggestion: You cannot enter 0\n");
+
+            }
+        } while (true);
     }
 
-    public static int quotient(int numerator, int denominator) {
+    public static int quotient(int numerator, int denominator) throws ArithmeticException {
 
-        return numerator/ denominator;
+        return numerator / denominator;
     }
 }
+
